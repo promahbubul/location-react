1f1df1678809eeafeaf28bca3affdf8ab70a6008
MAHBUBUL ALAM <33549330+promahbubul@users.noreply.github.com>
2024-10-17 23:48:48 +0600

    Update code

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,47 +6,32 @@ const Header = () => {
     lat: null,
     long: null,
   });
-  const [errorMessage, setErrorMessage] = useState(null);
 
-  useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          setCurrentLocation({
-            lat: position.coords.latitude,
-            long: position.coords.longitude,
-          });
-        },
-        (error) => {
-          setErrorMessage(
-            "Unable to retrieve location. Please enable location services and try again."
-          );
-          console.error(error);
-        }
-      );
-    } else {
-      setErrorMessage("Geolocation is not supported by this browser.");
-    }
-  }, []);
-
+  const handleLocation = () => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setCurrentLocation({
+          lat: position.coords.latitude,
+          long: position.coords.longitude,
+        });
+      },
+      (error) => console.error(error)
+    );
+  };
+  console.log(currentLocation);
   return (
     <div>
       <div className="max-w-7xl mx-auto mt-10 p-10 rounded-md w-full bg-slate-900 text-white">
-        {errorMessage ? (
-          <h1 className="text-xl font-bold">{errorMessage}</h1>
-        ) : (
-          <>
-            <h1 className="text-2xl font-bold">
-              Latitude: {currentLocation.lat}
-            </h1>
-            <h1 className="text-2xl font-bold">
-              Longitude: {currentLocation.long}
-            </h1>
-          </>
-        )}
+        <h1 className="text-2xl font-bold">{currentLocation.lat}</h1>
+        <h1 className="text-2xl font-bold">{currentLocation.long}</h1>
+        <button
+          onClick={handleLocation}
+          className="bg-blue-500 px-10 py-3 rounded-md"
+        >
+          Location BTN
+        </button>
       </div>
     </div>
   );
 };
-
 export default Header;
